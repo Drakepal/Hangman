f88b72046ddbd7e0ef957f367317564f8088dc09
Drakepal <playonplay23@gmail.com>
2023-02-06 14:46:15 -0600

    started working on gamestate, gamewords, and gamemanager

---
+++ app/src/main/java/com/example/hangman/GameManager.kt
@@ -0,0 +1,38 @@
+package com.example.hangman
+
+class GameManager {
+
+    private var lettersUsed: String = ""
+    private lateinit var underscoreWord: String
+    private lateinit var wordToGuess: String
+    private val maxTries = 7
+    private var currentTries = 0
+    private var drawable: Int = R.drawable.game0
+
+
+    fun generateUnderScores(word: String) {
+        val sb = java.lang.StringBuilder()
+        word.forEach { char ->
+            if (char == '/') {
+                sb.append('/')
+            } else {
+                sb.append("_")
+            }
+        }
+        underscoreWord = sb.toString()
+    }
+
+    private fun getHangmanDrawable(): Int {
+        return when (currentTries) {
+            0 -> R.drawable.game0
+            1 -> R.drawable.game1
+            2 -> R.drawable.game2
+            3 -> R.drawable.game3
+            4 -> R.drawable.game4
+            5 -> R.drawable.game5
+            6 -> R.drawable.game6
+            7 -> R.drawable.game7
+            else -> R.drawable.game7
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hangman/GameWords.kt
@@ -0,0 +1,26 @@
+package com.example.hangman
+
+object GameWords {
+    val words = listOf(
+        "polish",
+        "bleed",
+        "transfer",
+        "heavy",
+        "cellar",
+        "temporary",
+        "telling",
+        "flippant",
+        "needless",
+        "stormy",
+        "torpid",
+        "bone",
+        "crack",
+        "roomy",
+        "quarrel",
+        "input",
+        "jump",
+        "initiate",
+        "obey",
+        "remove"
+    )
+}
